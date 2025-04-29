c76cd53e15a8a0c3e6dadaef3cc97c2a55d01ef5
2504144 <khoi.2008.au@gmail.com>
2025-04-29 14:16:00 -0600

    working on 3D arrays and creating the cube

--- sketch.js
+++ sketch.js
@@ -1,28 +1,64 @@
-// Project Title
-// Your Name
-// Date
+// Rubiks Cube Similator
+// Khoi Tran
+// April 29th, 2025
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+
 //nice reference - https://www.youtube.com/watch?v=W24xhB9PO54
-let cube = {
-  front,
-  back,
-  left,
-  right,
+
+class Box{
+  constructor(x, y, z, sideLength, color){
+    this.sideLength = 20;
+    this.x = x * sideLength;
+    this.y = y * sideLength;
+    this.z = z * sideLength;
+    this.color = color;
+  }
+
+  display(){
+    fill(this.color);
+    stroke(0);
+    strokeWeight(8);
+    applyMatrix();
+    translate(this.x, this.y, this.z);
+    cube(this.sideLength);
+    resetMatrix();
+  }
 };
+
+let dimensions = 3;
+let boxes = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
-  frameRate(7);
+  orbitControl();
+  let cube = new Box(dimensions,dimensions, dimensions);
+
+  for(let i = 0; i < dimensions; i++){
+    for(let j = 0; j < dimensions; j++){
+      for(let k = 0; k < dimensions; k++){
+        let sideLength = 20;
+        let x = sideLength * i;
+        let y = sideLength * j;
+        let z = sideLength * k;
+        cube[[i][j][k]] = new Box(x, y, z, sideLength);
+
+      }
+    }
+  }
 }
 
 function draw() {
   background("black");
-  if (mouseIsPressed){
-    rotateX(mouseX);
-    rotateY(mouseY);
+
+  for(let i = 0; i < dimensions; i++){
+    for(let j = 0; j < dimensions; j++){
+      for(let k = 0; k < dimensions; k++){
+        cube[i][j][k].display();
+
+      }
+    }
   }
-  box(200, 200, 200);
 }
