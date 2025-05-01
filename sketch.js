069130bbabd2dbdb1c036e05ad5559c534d8cae4
2504144 <khoi.2008.au@gmail.com>
2025-05-01 13:22:28 -0600

    used orbit control function for moving around cube and working on load font

--- sketch.js
+++ sketch.js
@@ -4,6 +4,7 @@
 //
 // Extra for Experts:
 // - I learnt about 3D arrays
+// - Used orbitControl function added it to set up first but it does work in draw function
 
 
 //nice reference - https://www.youtube.com/watch?v=W24xhB9PO54
@@ -18,9 +19,11 @@ class Box{
 
   display(){
     push();
+
     //changing origin
     translate(this.x, this.y, this.z);
 
+    fill("white");
     stroke(0);
     strokeWeight(1);
     box(this.sideLength);
@@ -32,19 +35,21 @@ let boxes = [];
 let sideLength = 75;
 let dimensions = 3;
 
+let myFont;
+
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
-  
+
   //3 by 3 grid
   for(let i = 0; i < dimensions; i++){
     for(let j = 0; j < dimensions; j++){
       for(let k = 0; k < dimensions; k++){
         
-        //centering cube
+        //centering cubes
         let x = i * sideLength - sideLength;
         let y = j * sideLength - sideLength;
         let z = k * sideLength - sideLength;
-
+        
         boxes.push (new Box(x, y, z, sideLength));
       }
     }
@@ -52,12 +57,26 @@ function setup() {
 }
 
 function draw() {
-  background("black");
+  background(255, 255, 204);//color code - #FFFFCC
 
-  rotateX(mouseY * - 0.01);
-  rotateY(mouseX * - 0.01);
+  //able to move while mouse dragged
+  orbitControl();
 
   for(let box of boxes){
     box.display();
   }
+
+  title();
+}
+
+function preload(){
+  myFont = loadFont("King 500.ttf");
+}
+
+function title(){
+  fill(204, 204, 255);//color code - #CCCCFF
+  textAlign(CENTER,CENTER);
+  textFont(myFont);
+  textSize(50);
+  text("Rubiks Cube Simulator", width/2, height/7);
 }
